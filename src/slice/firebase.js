db262eaaf27dc7857ac9be769d1160c6dd15cea9
vatsal talaviya <vatsaltalaviyaa@gmail.com>
2025-07-23 12:08:42 +0530

    add api key in env for security

--- src/slice/firebase.js
+++ src/slice/firebase.js
@@ -3,8 +3,11 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 
+
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCW6vF4gYzMqw2BtVc9i0IZQaiOVAzJ8dw",
+  apiKey: `${import.meta.env.VITE_FIREBASE_APIKEY}`,
   authDomain: "rupee-root-5f259.firebaseapp.com",
   projectId: "rupee-root-5f259",
   storageBucket: "rupee-root-5f259.firebasestorage.app",
@@ -12,7 +15,7 @@ const firebaseConfig = {
   appId: "1:494116055983:web:27658a67584034ef8c9d5a",
   measurementId: "G-QYRMGLLB7D"
 };
-
+console.log(firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
