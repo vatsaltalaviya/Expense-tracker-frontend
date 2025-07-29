a4ec4d73e0c0560ea6db73ebbe442b8ec05462fd
vatsal talaviya <vatsaltalaviyaa@gmail.com>
2025-07-29 16:36:35 +0530

    small changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -150,20 +150,7 @@ const Dashboard = () => {
     });
   }, [expense]);
 
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
+ 
 
   const formatChartData = (data) => {
     return data?.map(({ name, income, expense }) => ({
